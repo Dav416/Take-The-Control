f9a790752dc53635a0aee0ccd93f8d5d8694bcdb
Dav416 <84945215+Dav416@users.noreply.github.com>
2025-03-24 17:09:20 -0500

    feat(Tailwind): some styles

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-     Hello world!
-    </h1>
-  </div>
+<div>
+  <h1>
+    Hello Word
+  </h1>
+</div>
 </template>
 
 <style scoped>
