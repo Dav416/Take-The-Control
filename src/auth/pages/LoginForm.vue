25f1f99597402d13a5cf5c316094691b6aff12ce
Dav416 <84945215+Dav416@users.noreply.github.com>
2025-03-29 12:00:56 -0500

    feat(LoginForm): new items

--- src/auth/pages/LoginForm.vue
+++ src/auth/pages/LoginForm.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        <h1>Hello World</h1>
+    <div class="flex justify-center">
+        <FormImage />
+        <div class="h-screen w-screen">
+            <h1 class="text-6xl text-center">Iniciar Sesión</h1>
+            <h4>¡Bienvenido! Por favor, ingresa con tu cuenta.</h4>
+            <FormInput />
+            <FormInput />
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
+import FormImage from '../components/FormImage.vue';
+import FormInput from '../components/FormInput.vue';
 
 </script>
 <style scoped>
