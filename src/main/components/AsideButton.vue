03fd0b8c67eb9c9e49a937fe8df8016d64c50d82
Dav416 <84945215+Dav416@users.noreply.github.com>
2025-06-24 16:51:21 -0500

    style(AsideButton): modify component hover

--- src/main/components/AsideButton.vue
+++ src/main/components/AsideButton.vue
@@ -3,7 +3,7 @@
     :href="link"
     v-bind="$attrs"
     :class="[
-      'flex w-full items-center py-4 mb-5 transition-colors duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-[var(--color-tc-white)] dark:hover:text-[var(--color-tc-blue-green)]',
+      'flex w-full items-center py-4 mb-5 transition-colors duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-[var(--color-tc-white)] dark:hover:text-[var(--color-tc-blue-green)] dark:hover:font-bold',
       $attrs.class ?? 'text-gray-900 dark:text-white'
     ]"
   >
